351359f846e00b8f2aa9cbe800d71219ea4ba96b
unknown <inf2065_usr@inf-2065-14.unwe.bg>
2014-05-17 12:59:49 +0300

    jjjkj

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("vkunevaappharbor")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("UNWE")> 
+<Assembly: AssemblyProduct("vkunevaappharbor")> 
+<Assembly: AssemblyCopyright("Copyright © UNWE 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f9686821-7d89-48d4-843f-fed42da7bf4e")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
